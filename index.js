174a29976f04d5b14b41a21648ed98c7502117da
sachin patwal <46517602+Sachinpatwal07@users.noreply.github.com>
2020-03-30 18:29:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 $(document).on("keypress",function(event){
-var randomNumber1=Math.floor(Math.random()*6)+1;
-var randomNumber2=Math.floor(Math.random()*6)+1;
 
-var imgage1="images/"+"dice"+randomNumber1+".png";
-var imgage2="images/"+"dice"+randomNumber2+".png";
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var rand1=document.querySelectorAll("img")[0];
-rand1.setAttribute("src", imgage1);
+  var randomDiceImage = "dice" + randomNumber1 + ".png";
 
-var rand2=document.querySelectorAll("img")[1];
-rand2.setAttribute("src",imgage2);
+  var randomImageSource = "images/" + randomDiceImage;
 
+  var image1 = document.querySelectorAll("img")[0];
+
+  image1.setAttribute("src", randomImageSource);
 
 
-if(randomNumber1>randomNumber2)
-document.querySelector("h1").innerHTML="😜 Player 1 Wins ";
-else if(randomNumber1<randomNumber2)
-document.querySelector("h1").innerHTML="Player 2 Wins 😜";
-else {
-document.querySelector("h1").innerHTML="Draw";
-}
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+  document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+
+  if (randomNumber1 > randomNumber2) {
+  $("h1").text("🚩 Play 1 Wins!");
+  }
+  else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  }
+  else {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
+
+
 })
